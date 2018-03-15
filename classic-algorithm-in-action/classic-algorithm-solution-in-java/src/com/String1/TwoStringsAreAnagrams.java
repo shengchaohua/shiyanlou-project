595b3056aabac84f87fd1517ab869cafd54cc31e
shengchaohua <shengchaohua163@163.com>
2018-03-15 20:26:45 +0800

    add lintcode no.158 Two Strings Are Anagrams

---
+++ classic-algorithm-in-action/classic-algorithm-solution-in-java/src/com/String1/TwoStringsAreAnagrams.java
@@ -0,0 +1,31 @@
+package com.String1;
+
+public class TwoStringsAreAnagrams {
+	public boolean anagram(String s, String t) {
+        // write your code here
+        if (s == null || t == null || s.length() != t.length()) {
+			return false;
+		}
+		int[] flag = new int[256];
+		int length = s.length();
+		char[] charArr1 = s.toCharArray();
+		char[] charArr2 = t.toCharArray();
+		for (int i = 0; i < length; i++) {
+			flag[charArr1[i]]++;
+			flag[charArr2[i]]--;
+		}
+		for (int i = 0; i < flag.length; i++) {
+			if (flag[i] != 0) {
+				return false;
+			}
+		}
+		return true;
+    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TwoStringsAreAnagrams two = new TwoStringsAreAnagrams();
+		System.out.println(two.anagram("abcd", "acbd"));
+	}
+
+}
