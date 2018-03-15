5b4537ce55c2f3217682d01d70f660836005d8d1
shengchaohua <shengchaohua163@163.com>
2018-03-15 21:01:28 +0800

    add lintcode No.55 Compare Strings

---
+++ classic-algorithm-in-action/classic-algorithm-solution-in-python/CompareStrings.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 15 20:45:38 2018
+
+@problem: liintcode No.55 Compare Strings
+
+@author: sheng
+"""
+
+class Solution:
+    """
+    @param A: A string
+    @param B: A string
+    @return: if string A contains all of the characters in B return true else return false
+    """
+    def compareStrings(self, A, B):
+        import collections
+        letters = collections.Counter(A)
+        
+        for b in B:
+            if b not in letters:
+                return False
+            elif letters[b] <= 0:
+                return False
+            else:
+                letters[b] -= 1
+        return True
+
+def  main():
+    s = Solution()
+    print(s.compareStrings('ABCD', 'AABC'))
+    
+if __name__ == '__main__':
+    main()
