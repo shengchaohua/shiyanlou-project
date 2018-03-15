e63c63cfee72d4861b35213383d3e320c87ac88f
shengchaohua <shengchaohua163@163.com>
2018-03-15 21:38:25 +0800

    add lintcode No.55 Compare Strings

---
+++ classic-algorithm-in-action/classic-algorithm-solution-in-java/src/com/String1/CompareStrings.java
@@ -0,0 +1,37 @@
+package com.String1;
+
+public class CompareStrings {
+	/**
+     * @param A : A string includes Upper Case letters
+     * @param B : A string includes Upper Case letter
+     * @return :  if string A contains all of the characters in B return true else return false
+     */
+    public boolean compareStrings(String A, String B) {
+        // write your code here
+        if (A == null && B == null || A.length() < B.length()) {
+			return false;
+		}
+		int[] flag = new int[256];
+		int length = A.length();
+		char[] charArr1 = A.toCharArray();
+		char[] charArr2 = B.toCharArray();
+		for (int i = 0, j = 0; i < length; i++) {
+			flag[charArr1[i]]++;
+			if (j < B.length()) {
+				flag[charArr2[j]]--;
+				j++;
+			}
+		}
+		for (int i = 0; i < flag.length; i++) {
+			if (flag[i] < 0) {
+				return false;
+			}
+		}
+		return true;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
