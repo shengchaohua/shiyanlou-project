0fc1925f0087b7aa40086a9e7654671d2798fcc5
shengchaohua <shengchaohua163@163.com>
2018-03-19 14:23:23 +0800

    modify leetcode 58 & lintcode 422 Length of Last Word

--- classic-algorithm-in-action/classic-algorithm-solution-in-java/src/com/String/LengthofLastWord.java
+++ classic-algorithm-in-action/classic-algorithm-solution-in-java/src/com/String/LengthofLastWord.java
@@ -1,7 +1,20 @@
 package com.String;
 
 public class LengthofLastWord {
-
+	/**
+     * @param s A string
+     * @return the length of last word
+     */
+    public int lengthOfLastWord(String s) {
+        // Write your code here
+        String trimedS = s.trim();
+		if (trimedS.equals("")) {
+			return 0;
+		}
+		String[] sArr = s.split(" ");
+		return sArr[sArr.length-1].length();
+    }
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
