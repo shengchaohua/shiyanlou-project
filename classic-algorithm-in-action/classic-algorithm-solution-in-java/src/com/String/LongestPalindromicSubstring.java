ca4064cc70ffcb0f799c6997f758ab348ab96c21
shengchaohua <shengchaohua163@163.com>
2018-03-19 10:14:46 +0800

    modify leecode 5 & lintcde 200 Longest Palindromic Substring

--- classic-algorithm-in-action/classic-algorithm-solution-in-java/src/com/String/LongestPalindromicSubstring.java
+++ classic-algorithm-in-action/classic-algorithm-solution-in-java/src/com/String/LongestPalindromicSubstring.java
@@ -1,12 +1,15 @@
 package com.String;
 
+import javax.print.DocFlavor.STRING;
+
 public class LongestPalindromicSubstring {
 	/*
      * @param s: input string
      * @return: the longest palindromic substring
      */
     public String longestPalindrome(String s) { 
-    	// This solution is right. But both lintcode and leetcode Time Exceed! 
+    	// This solution is right. But both lintcode and leetcode Time Exceed!
+    	// O(n^3)
         String result = new String();
         if (s == null || s.isEmpty()) return result;
 
@@ -37,11 +40,42 @@ public class LongestPalindromicSubstring {
         return true;
     }
     
+    public String longestPalindromeBetter(String s) {
+    	// 假定扫描的每个字母是回文的中间位置（需要处理奇偶两种情况）
+    	// 从该位置向两头搜索寻找最大回文长度
+    	// O(n^2)
+    	if (s.isEmpty()) {
+    		return s;
+    	}
+    	
+    	String res = "";
+    	for (int i = 0; i < s.length(); i++) {
+    		String temp = "";
+    		temp = getPali(s, i, i);
+    		if (temp.length() > res.length())
+    			res = temp;
+    		
+    		temp = getPali(s, i, i+1);
+    		if (temp.length() > res.length())
+    			res = temp;
+    	}
+    	return res;
+    }
+    
+    private String getPali(String s, int l, int r) {
+    	while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+    		l--;
+    		r++;
+    	}
+    	return s.substring(l+1, r);	
+    }
+    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		LongestPalindromicSubstring l = new LongestPalindromicSubstring();
-		String s = "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth";
+		String s = "babad";
 		System.out.println(l.longestPalindrome(s));
+		System.out.println(l.longestPalindromeBetter("bababababababa"));
 	}
 
 }
--- classic-algorithm-in-action/classic-algorithm-solution-in-python/LongestPalindromicSubstring.py
+++ classic-algorithm-in-action/classic-algorithm-solution-in-python/LongestPalindromicSubstring.py
@@ -13,22 +13,56 @@ class Solution:
     @return: the longest palindromic substring
     """
     def longestPalindrome(self, s): 
+        # 最简单的方案，穷举所有可能的子串，判断子串是否为回文，使用一变量记录最大回文长度，
+        # 若新的回文超过之前的最大回文长度则更新标记变量并记录当前回文的起止索引，最后返回最长回文子串
         # This solution is right. Lintcode AC while leetcode NOT.
+        # O(n^3)
         if not s:
-            return ""
+            return s
 
         n = len(s)
         longest, left, right = 0, 0, 0
         for i in range(0, n):
             for j in range(i + 1, n + 1):
                 substr = s[i:j]
-                if self.isPalindrome(substr) and len(substr) > longest:
+                if self.__isPalindrome(substr) and len(substr) > longest:
                     longest = len(substr)
                     left, right = i, j
         return s[left:right]
 
-    def isPalindrome(self, s):
+    def __isPalindrome(self, s):
         if not s:
             return False
-        # reverse compare
         return s == s[::-1]
+    
+    def longestPalindromeBetter(self, s):
+        # 假定扫描的每个字母是回文的中间位置（需要处理奇偶两种情况）
+        # 从该位置向两头搜索寻找最大回文长度
+        # O(n^2)
+        if not s:
+            return s
+        
+        res = ""
+        for i in range(len(s)):
+            temp = ""
+            temp = self.__getPali(s, i, i)
+            if len(temp) > len(res):
+                res = temp
+            temp = self.__getPali(s, i, i+1)
+            if len(temp) > len(res):
+                res = temp
+        return res
+    
+    def __getPali(self, s, l, r):
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            l -= 1
+            r += 1
+        return s[l+1:r]
+
+def main():
+    s = Solution()
+    print(s.longestPalindrome("babad"))
+    print(s.longestPalindromeBetter("babad"))
+    
+if __name__ == '__main__':
+    main()
