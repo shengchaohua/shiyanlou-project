cae0b04ebe331833fc3022856cbed42f0b64a259
shengchaohua <shengchaohua163@163.com>
2018-03-20 19:32:30 +0800

    add lintcode 50 Product of Array Exclude Itself

--- classic-algorithm-in-action/classic-algorithm-solution-in-python/ProductOfArray.py
+++ classic-algorithm-in-action/classic-algorithm-solution-in-python/ProductOfArray.py
@@ -16,13 +16,16 @@ class Solution:
         p = 1
         n = len(nums)
         result = []
+        
         for i in range(0,n):
             result.append(p)
             p = p * nums[i]
+        
         p = 1
         for i in range(n-1,-1,-1):
             result[i] = result[i] * p
             p = p * nums[i]
+        
         return result
     
         
