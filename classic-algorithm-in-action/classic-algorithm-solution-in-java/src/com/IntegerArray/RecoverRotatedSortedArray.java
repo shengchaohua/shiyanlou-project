c345717715e82cf9a88f07e597dd030e0327fbf0
shengchaohua <shengchaohua163@163.com>
2018-03-20 18:04:41 +0800

    modify lintcode 39. Recover Rotated Sorted Array

--- classic-algorithm-in-action/classic-algorithm-solution-in-java/src/com/IntegerArray/RecoverRotatedSortedArray.java
+++ classic-algorithm-in-action/classic-algorithm-solution-in-java/src/com/IntegerArray/RecoverRotatedSortedArray.java
@@ -1,5 +1,42 @@
 package com.IntegerArray;
 
-public class RecoverRotatedSortedArray {
+import java.util.ArrayList;
 
+public class RecoverRotatedSortedArray {
+	/**
+     * @param nums: The rotated sorted array
+     * @return: The recovered sorted array
+     */
+    public void recoverRotatedSortedArray(ArrayList<Integer> nums) {
+        if (nums == null || nums.size() <= 1) {
+            return;
+        }
+
+        int pos = 1;
+        while (pos < nums.size()) { // find the break point
+            if (nums.get(pos - 1) > nums.get(pos)) {
+                break;
+            }
+            pos++;
+        }
+        rotate(nums, 0, pos - 1);
+        rotate(nums, pos, nums.size() - 1);
+        rotate(nums, 0, nums.size() - 1);
+    }
+
+    private void rotate(ArrayList<Integer> nums, int left, int right) { // in-place rotate
+        while (left < right) {
+            int temp = nums.get(left);
+            nums.set(left, nums.get(right));
+            nums.set(right, temp);
+            left++;
+            right--;
+        }
+    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
 }
