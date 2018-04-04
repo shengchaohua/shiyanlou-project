cb1f2791ddc0175a769c9a6af362976af5c539a9
shengchaohua <shengchaohua163@163.com>
2018-04-04 19:06:57 +0800

    modify lintcode 83. Single Number II

--- classic-algorithm-in-action/classic-algorithm-solution-in-python/SingleNumberII.py
+++ classic-algorithm-in-action/classic-algorithm-solution-in-python/SingleNumberII.py
@@ -13,7 +13,6 @@ class Solution:
         @param A: An integer array
         @return: An integer
         """
-        # write your code here
         if not A:
             return 0
         
@@ -22,10 +21,16 @@ class Solution:
             bitsum = 0
             for num in A:
                 bitsum += ((num >> i) & 1)
-            res += (bitsum % 3) << i
+            res |= (bitsum % 3) << i
         
-        return res
+        return self._twos_comp(res, 32)
     
+    def _twos_comp(self, val, bits):
+        """
+        compute the 2's compliment of int value val
+        e.g. -4 ==> 11100 == -(10000) + 01100
+        """
+        return -(val & (1 << (bits - 1))) | (val & ((1 << (bits - 1)) - 1))
     
 def main():
     s = Solution()
