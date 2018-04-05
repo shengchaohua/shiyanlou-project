44fe61632ff02be42103745e3e3e6b14dc54a15c
shengchaohua <shengchaohua163@163.com>
2018-04-05 16:50:34 +0800

    add leetcode 137. & lintcode 83. Single Number

---
+++ classic-algorithm-in-action/classic-algorithm-solution-in-java/src/com/BitManipulation/SingleNumberII.java
@@ -0,0 +1,32 @@
+package com.BitManipulation;
+
+// problem: leetcode 137. & lintcode 83. Single Number
+public class SingleNumberII {
+	/**
+     * @param A: An integer array
+     * @return: An integer
+     */
+    public int singleNumberII(int[] A) {
+        if (A == null || A.length == 0) {
+        	return 0;
+        }
+        
+        int res = 0;
+        int bitsum;
+        for (int i = 0; i < 8 * Integer.BYTES; i++) {
+        	bitsum = 0;
+        	for (int j = 0; j < A.length; j++) {
+        		bitsum += ((A[j] >> i) & 1);
+        	}
+        	res = res | ((bitsum % 3) << i); // µÈ¼ÛÓÚ res = res + ((bitsum % 3) << i); 
+        }
+        
+        return res;
+    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
