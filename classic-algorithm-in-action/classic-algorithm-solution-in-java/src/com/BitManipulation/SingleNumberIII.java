8e159b0afbad67048e62325729fef76b0d6a85f4
shengchaohua <shengchaohua163@163.com>
2018-04-05 17:42:27 +0800

    add lintcode 84. Single Number III

---
+++ classic-algorithm-in-action/classic-algorithm-solution-in-java/src/com/BitManipulation/SingleNumberIII.java
@@ -0,0 +1,42 @@
+package com.BitManipulation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// problem: lintcode 84. Single Number III
+public class SingleNumberIII {
+	/**
+     * @param A : An integer array
+     * @return : Two integers
+     */
+    public List<Integer> singleNumberIII(int[] A) {
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        if (A == null || A.length == 0) return nums;
+
+        int x1xorx2 = 0;
+        for (int i : A) {
+            x1xorx2 ^= i;
+        }
+
+        // get the last 1 bit of x1xorx2, e.g. 1010 ==> 0010
+        int last1Bit = x1xorx2 - (x1xorx2 & (x1xorx2 - 1));
+        int single1 = 0, single2 = 0;
+        for (int i : A) {
+            if ((last1Bit & i) == 0) {
+                single1 ^= i;
+            } else {
+                single2 ^= i;
+            }
+        }
+
+        nums.add(single1);
+        nums.add(single2);
+        return nums;
+    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
