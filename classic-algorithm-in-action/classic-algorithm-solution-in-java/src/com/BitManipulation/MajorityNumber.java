4f9bdb4fa192780346c15c2b8822782bf411e329
shengchaohua <shengchaohua163@163.com>
2018-04-06 18:04:45 +0800

    add leetcode 169. Majority Element & lintcode 46. Majority Number

---
+++ classic-algorithm-in-action/classic-algorithm-solution-in-java/src/com/BitManipulation/MajorityNumber.java
@@ -0,0 +1,54 @@
+package com.BitManipulation;
+
+import java.util.ArrayList;
+
+// problem: leetcode 169. Majority Element & lintcode 46. Majority Number
+public class MajorityNumber {
+	public int majorityElement(int [] nums) {
+		// leetcode AC!
+		int count = 0;
+		int res = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (count == 0) {
+				res = nums[i];
+				count++;
+			} else {
+				if (nums[i] == res) {
+					count++;
+				} else {
+					count--;
+				}
+			}
+		}
+		return res;
+	}
+	
+	/**
+     * @param nums: a list of integers
+     * @return: find a  majority number
+     */
+    public int majorityNumber(ArrayList<Integer> nums) {
+        // lintcode AC!
+        int count = 0;
+		int res = 0;
+		for (int i = 0; i < nums.size(); i++) {
+			if (count == 0) {
+				res = nums.get(i);
+				count = 1;
+			} else {
+				if (nums.get(i) == res) {
+					count++;
+				} else {
+					count--;
+				}
+			}
+		}
+		return res;
+    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ classic-algorithm-in-action/classic-algorithm-solution-in-python/MajorityNumber.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  6 18:01:20 2018
+
+@problem: leetcode 169. Majority Element & lintcode 46. Majority Number
+
+@author: sheng
+"""
+
+class Solution:
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # leetcode & lintcode AC!
+        res = 0
+        count = 0
+        for n in nums:
+            if count == 0:
+                res = n
+                count += 1
+            else:
+                count = count + 1 if res == n else count - 1
+        return res
