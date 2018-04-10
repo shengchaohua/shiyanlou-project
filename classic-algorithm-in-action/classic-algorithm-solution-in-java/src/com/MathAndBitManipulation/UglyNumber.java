a326ff32bf7f9f838ec48128afe8f3ab35df95c3
shengchaohua <shengchaohua163@163.com>
2018-04-10 18:20:24 +0800

    add leetcode 263. Ugly Number

---
+++ classic-algorithm-in-action/classic-algorithm-solution-in-java/src/com/MathAndBitManipulation/UglyNumber.java
@@ -0,0 +1,30 @@
+package com.MathAndBitManipulation;
+
+// problem: leetcode 263. Ugly Number
+public class UglyNumber {
+	public boolean isUgly(int num) {
+		// AC
+		if (num == 0)
+			return false;
+		
+		while (num % 2 == 0) {
+			num = num >> 1;
+		}
+		while (num % 3 == 0) {
+			num = num / 3;
+		}
+		while (num % 5 == 0) {
+			num = num / 5;
+		}
+
+		return num == 1;
+	}
+
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
